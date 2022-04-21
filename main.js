1dfc45ec372f15a3b304f40a0113d2fa880231c3
Jonathon Surratt <jsurratt@nr.edu>
2022-04-21 12:03:25 -0400

    Added collision! Yay!

--- main.js
+++ main.js
@@ -25,6 +25,12 @@ class Rain {
   get getCanvas() {
     return this.canvas;
   }
+  get getSize(){
+    return this.size;
+  }
+  set setSize(new_size){
+    this.size = new_size;
+  }
   //draws a rectangle at object's coordinates
   spawnRain() {
     this.ctx.fillRect(this.x, this.y, this.size, this.size);
@@ -34,12 +40,69 @@ class Rain {
     this.y += 5;
     this.spawnRain();
   }
+  slideRight(){
+    this.x += 5;
+    this.spawnRain();
+  }
+  slideLeft(){
+    this.x -= 5;
+    this.spawnRain();
+  }
 }
+
+class Rectangle{
+  constructor(x, y, width, height){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  get getX(){
+    return this.x
+  }
+  set setX(new_x){
+    this.x = new_x;
+  }
+  get getY(){
+    return this.y;
+  }
+  set setY(new_y){
+    this.y = new_y;
+  }
+  get getWidth(){
+    return this.width;
+  }
+  set setWidth(new_width){
+    this.width = new_width;
+  }
+  get getHeight(){
+    return this.height;
+  }
+  set setHeight(new_height){
+    this.height = new_height;
+  }
+
+}
+let plaform;
+let platform2;
 
 function init() {
   let canvas = document.getElementById("canvas");
   canvas.height = window.innerHeight * 0.7;
   canvas.width = window.innerWidth * 0.7;
+  platform = new Rectangle(
+    canvas.width / 2 - 100, 
+    canvas.height / 2 - 50,
+    200,
+    50
+    )
+  platform2 = new Rectangle(
+    canvas.width + 50, 
+    canvas.height + 100,
+    200,
+    50
+    )
 }
 
 //create global array to store raindrop objects in
@@ -84,11 +147,22 @@ function makeRainFall() {
   drawPlatform();
   //draw each new raindrop
   for (let i = 0; i < rainDrops.length; i++) {
-    rainDrops[i].rainFall();
-    //if raindrop is offscreen, remove it from the array
-    if (rainDrops[i].getY > window.innerHeight) {
-      rainDrops.splice(i, 1);
-    }
+    if(rainDrops[i].getX + rainDrops[i].getSize - 1 > platform.getX &&
+      rainDrops[i].getX < platform.getX + platform.getWidth &&
+      rainDrops[i].getY + rainDrops[i].getSize - 1> platform.getY &&
+      rainDrops[i].getY < platform.getY + platform.getHeight){
+        if(rainDrops[i].getX < platform.getX + platform.getWidth / 2){
+          rainDrops[i].slideLeft();
+        }else{
+          rainDrops[i].slideRight();
+        }
+    }else{
+        rainDrops[i].rainFall();
+        //if raindrop is offscreen, remove it from the array
+        if (rainDrops[i].getY > window.innerHeight) {
+          rainDrops.splice(i, 1);
+        }
+      }
   }
   //call a new frame every 33 ms until rain is stopped
   if (rainStarted) {
@@ -100,10 +174,10 @@ function drawPlatform() {
   //draw a brown platform in the middle of the screen
   rainDrops[0].getCtx.fillStyle = "rgb(130, 65, 0)";
   rainDrops[0].getCtx.fillRect(
-    rainDrops[0].getCanvas.width / 2 - 100,
-    rainDrops[0].getCanvas.height / 2 - 50,
-    200,
-    50
+    platform.getX,
+    platform.getY,
+    platform.getWidth,
+    platform.getHeight
   );
   rainDrops[0].getCtx.fillStyle = "rgba(0, 0, 255, 0.5)";
 }
